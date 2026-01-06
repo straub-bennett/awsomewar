67ee7c63d65b861fdfe1857e277d05fc3adaa9d5
Bennett <straubb@go.oes.edu>
2026-01-06 12:40:42 -0800

    Real first day, pick up and do random events

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import random
 dice1 = random.randint(1, 6)
 dice2 = random.randint(1, 20)
 dice3 = random.randint(1, 50)
+countrys = {'Austria':4,
+            'prussia':7,
+            'Russia':8,
+            'France':10,
+            'Denmark':2,
+            'Switzerland':1,
+            'Italy':3,
+            'Ottoman Empire':6,
+            'Spain':4}
 
 while x == True:
     try:
@@ -16,31 +25,31 @@ while x == True:
         print('An error occurred. Please try again.')
 y = True
 while y == True:
-    country = input('What country do you want to play as?  (Prussia/easy, Austrian empire/hard, Ottoman Empire/insane): ')
-    if country.lower() == 'prussia' or country.lower() == 'easy':
+    country = input('What country do you want to play as?  (Prussia/easy, Austria/hard, Ottoman Empire/insane): ')
+    if country.lower() == 'prussia':
         print('You have chosen Prussia with easy difficulty.')
         y = False
         money = 30000
         army = 10000
         stability = 35
         culture = 'German'
-        Neighbors = ['Austria-Hungary', 'Russia', 'France', 'Denmark', 'Switzerland']
-    elif country.lower() == 'austria' or country.lower() == 'hard':
+        Neighbors = [countrys['Austria'], countrys['Russia'], countrys['France'], countrys['Denmark'], countrys['Switzerland']]
+    elif country.lower() == 'austria':
         print('You have chosen Austria with hard difficulty.')
         y = False
         money = 20000
         army = 4000
         stability = 10
         culture = 'austrian/hungarian'
-        Neighbors = ['Prussia', 'Russia', 'Italy', 'Ottoman Empire']   
-    elif country.lower() == 'ottoman empire' or country.lower() == 'insane':
+        Neighbors = [countrys['Prussia'], countrys['Russia'], countrys['Italy'], countrys['Ottoman Empire']]  
+    elif country.lower() == 'ottoman empire':
         print('You have chosen Ottoman Empire with insane difficulty.')
         y = False
         money = 15000
         army = 1000
         stability = 5
         culture = 'Turkish'
-        Neighbors = ['Russia', 'Austria-Hungary', 'Italy', 'Bulgaria', 'Greece']
+        Neighbors = [countrys['Russia'], countrys['Austria'], countrys['Italy'], countrys['Spain']]
     else:
         print('Invalid choice. Please try again')
         continue
@@ -51,8 +60,8 @@ if tutorial.lower() == 'yes':
     print('In this game, you will manage your country by making strategic decisions to grow your economy, build your army, and maintain stability. Your goal is to become the hegimon of Europe and Asia. The game is turn based, and each turn you will have to make a stratigic desicion to grow your economy, militry, or stability. your economy represents how well you can do other actions such as recruting an army, or increaseing stabiliy, which pretty much represetns the health of your country. Your military represents how well you can defend yourself from other countries or attack them. Good luck and have fun!')
 else: 
     print('Skipping tutorial...')
-    game_start = True
+game_start = True
 while game_start == True:
-random_event = random.randint(1, 5)
-    if random_event == 1:
+    random_event = random.randint(1, 5)
+    if round(random_event + stability/30) == 1:
         print('Random event: ')
